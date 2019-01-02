ab16f478d31a43412dd7b7cd4b06567da288157f
duanshuting <1579455685@qq.com>
2019-01-02 19:53:20 +0800

    完成了删除功能

--- src/components/users.vue
+++ src/components/users.vue
@@ -9,10 +9,11 @@
     <!-- 搜索框 + 添加按钮 -->
     <el-row>
       <el-col>
-        <el-input placeholder="请输入内容" v-model="query" class="input-with-select">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-input @clear="getAllUsers()" placeholder="请输入用户名" v-model="query" class="input-with-select" clearable>
+          <el-button slot="append" icon="el-icon-search"
+          @click="searchUsers()"></el-button>
         </el-input>
-        <el-button type="primary">添加用户</el-button>
+        <el-button type="primary" @click="showDiaAdd()">添加用户</el-button>
       </el-col>
     </el-row>
     <!-- 表格 
@@ -20,7 +21,7 @@
         每一列 el-table-column 中 label是表头文字，prop是该列中每一行的数据名
         prop 的数据来源是 el-table 的data属性值 [{prop要用的数据名name：值}] name
     -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table height=300px :data="tableData" style="width: 100%">
       <el-table-column prop="id" label="#" width="120"></el-table-column>
       <el-table-column prop="username" label="姓名" width="160"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="200"></el-table-column>
@@ -51,12 +52,53 @@
         <template slot-scope="scope">
           <el-row>
             <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
-            <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle size="mini" plain @click="showDeleConfirm(scope.row)"></el-button>
             <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
           </el-row> 
         </template>
       </el-table-column>
     </el-table>
+
+    <!-- 分页 -->
+    <!-- 
+      @size-change    每页显示条数改变时
+      @current-change 当前页码改变时
+      current-page    当前页码
+      page-sizes      控制每页显示条数所在的数组[2,4,6,8]
+      layout          分页组件的小功能
+      total           数组总个数
+     -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="2"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+
+    <!-- 添加用户对话框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="formLabelWidth">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="formLabelWidth">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -66,9 +108,18 @@ export default {
     return {
       query: "",
       tableData: [],
-      query: "",
       pagenum: 1,
-      pagesize: 2
+      pagesize: 2,
+      total: -1,
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      formLabelWidth: "100px",
+      // 添加对话框的显示与隐藏
+      dialogFormVisibleAdd: false
     };
   },
   created() {
@@ -76,8 +127,70 @@ export default {
     this.getTableData()
   },
   methods: {
+
+    // 点击删除用户 - 打开提示框
+    showDeleConfirm(user) {
+      // console.log(user)
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 发送删除的请求
+        const res = await this.$http.delete(`users/${user.id}`)
+        const {meta:{msg,status}} = res.data
+        if (status === 200) {
+          this.$message.success(msg)
+          this.pagenum = 1
+          this.getTableData()
+        }
+      }).catch(() => {
+        this.$message.warning("取消删除") 
+      }); 
+    },
+
+    // 添加用户 - 发送请求
+    async addUser() {
+      // 1. 发送请求
+      const res = await this.$http.post(`users`, this.form)
+      const {meta:{msg,status}} = res.data
+      // console.log(res)
+      if (status === 201) {
+        // 2. 关闭对话框
+        this.dialogFormVisibleAdd = false
+        // 3. 刷新表格
+        this.getTableData()
+      } else {
+        // 提示框警告
+        this.$message.warning(msg)
+      }
+    },
+
+    // 点击添加用户按钮 - 显示对话框
+    showDiaAdd() {
+      this.form = {}
+      this.dialogFormVisibleAdd = true
+    },
+
+    // 分页组件相关的代码
+    /**
+     * 1. 当前页码改变时，按照新页码pagenum重新发请求
+     * 2. 当每页条数改变时，按照新pagesize重新发请求
+     */
+     handleSizeChange(val) {
+        console.log(`每页 ${val} 条`)
+        this.pagesize = val
+        this.pagenum = 1
+        this.getTableData()
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`)
+        this.pagenum = val
+        this.getTableData()
+      },
+
+    // 请求表格数据
     async getTableData() {
-    
       // 在发起请求(除了登录之外)之前，都需要授权 -> 设置请求头(axios文档)
       // 1. 设置请求头
       const AUTH_TOKEN = localStorage.getItem("token")
@@ -90,11 +203,23 @@ export default {
       if (status === 200) {
         // 3. 给表格数据赋值
         this.tableData = users
+        this.total = total
         // console.log(this.tableData)
         
         // 4. 提示框
         this.$message.success(msg)
       }
+    },
+
+    // 搜索用户
+    searchUsers() {
+      this.pagenum = 1
+      this.getTableData()
+    },
+
+    // 点击清空按钮时，重新获取数据
+    getAllUsers() {
+      this.getTableData()
     }
   }
 };

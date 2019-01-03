1f96ded2303ee05fbf3f72c8aceb5f0e66bc7a47
duanshuting <1579455685@qq.com>
2019-01-03 17:37:37 +0800

    完成了用户列表功能

--- src/components/users.vue
+++ src/components/users.vue
@@ -6,6 +6,7 @@
       <el-breadcrumb-item>活动管理</el-breadcrumb-item>
       <el-breadcrumb-item>活动列表</el-breadcrumb-item>
     </el-breadcrumb>
+
     <!-- 搜索框 + 添加按钮 -->
     <el-row>
       <el-col>
@@ -16,12 +17,13 @@
         <el-button type="primary" @click="showDiaAdd()">添加用户</el-button>
       </el-col>
     </el-row>
+    
     <!-- 表格 
         el-table 需要通过data属性提供数据源 tableData
         每一列 el-table-column 中 label是表头文字，prop是该列中每一行的数据名
         prop 的数据来源是 el-table 的data属性值 [{prop要用的数据名name：值}] name
     -->
-    <el-table height=300px :data="tableData" style="width: 100%">
+    <el-table height=300px :data="tableData">
       <el-table-column prop="id" label="#" width="120"></el-table-column>
       <el-table-column prop="username" label="姓名" width="160"></el-table-column>
       <el-table-column prop="email" label="邮箱" width="200"></el-table-column>
@@ -42,6 +44,7 @@
         <!-- 该单元格中的数据不是prop的值 true -->
         <template slot-scope="scope">
           <el-switch
+            @change="changeUserState(scope.row)"
             v-model="scope.row.mg_state"
             active-color="#13ce66"
             inactive-color="#ff4949">
@@ -51,9 +54,9 @@
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
           <el-row>
-            <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+            <el-button type="primary" icon="el-icon-edit" circle size="mini" plain @click="showEditDia(scope.row)"></el-button>
             <el-button type="danger" icon="el-icon-delete" circle size="mini" plain @click="showDeleConfirm(scope.row)"></el-button>
-            <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+            <el-button type="success" icon="el-icon-check" circle size="mini" plain @click="showDiaRole(scope.row)"></el-button>
           </el-row> 
         </template>
       </el-table-column>
@@ -99,6 +102,52 @@
         <el-button type="primary" @click="addUser()">确 定</el-button>
       </div>
     </el-dialog>
+
+    <!-- 编辑用户对话框 -->
+    <el-dialog title="编辑用户" :visible.sync="dialogFormVisibleEdit">
+      <el-form :model="form">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input disabled v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="formLabelWidth">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="formLabelWidth">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+        <el-button type="primary" @click="editUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 分配角色对话框 -->
+    <el-dialog title="分配角色" :visible.sync="dialogFormVisibleRole">
+      <el-form :model="form">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          {{curUserName}}
+        </el-form-item>
+        <el-form-item label="角色" :label-width="formLabelWidth">
+          <!-- 
+            1. v-model 绑定数据
+            2. v-model 绑定的数据如果和option的value值一样 -> 默认选项option
+            3. 点击不同的option时，v-model绑定的值就会变化 -> 变成了点击的option的value
+           -->
+           {{curUserRoleId}}
+          <el-select v-model="curUserRoleId">
+            <!-- 请选择 -->
+            <el-option label="请选择" :value="-1" disabled></el-option>
+            <!-- v-for遍历 -->
+            <el-option v-for="(v,i) in roles" :key="i" :label="v.roleName" :value="v.id"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleRole = false">取 消</el-button>
+        <el-button type="primary" @click="setRole()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -119,7 +168,13 @@ export default {
       },
       formLabelWidth: "100px",
       // 添加对话框的显示与隐藏
-      dialogFormVisibleAdd: false
+      dialogFormVisibleAdd: false,
+      dialogFormVisibleEdit: false,
+      dialogFormVisibleRole: false,
+      curUserRoleId: -1,
+      roles: [],
+      curUserName: "",
+      curUserId: -1
     };
   },
   created() {
@@ -127,6 +182,64 @@ export default {
     this.getTableData()
   },
   methods: {
+    // 分配角色 - 发送请求
+    async setRole() {
+      const res = await this.$http.put(`users/${this.curUserId}/role`, {
+        rid:this.curUserRoleId
+      })
+      // console.log(res);
+      this.dialogFormVisibleRole = false;
+    } ,
+
+    // 分配角色 - 显示对话框
+    async showDiaRole(user) {
+      // console.log(user);  // 没有当前用户的角色id
+      this.curUserName = user.username; 
+      this.curUserId = user.id;
+
+      // 获取所有的角色
+      const res = await this.$http.get(`roles`);
+      this.roles = res.data.data;
+      // console.log(this.roles);
+
+      // 要的是当前user用户的角色id -> 30
+      const res2 = await this.$http.get(`users/${user.id}`);
+      // console.log(res2)
+      this.curUserRoleId = res2.data.data.rid;
+
+      // 打开对话框
+      this.dialogFormVisibleRole = true;
+    },
+
+    // 修改用户状态
+    async changeUserState(user) {
+      const res = await this.$http.put(`users/${user.id}/state/${user.mg_state}`)
+      // console.log(res);
+    },
+
+    // 编辑用户 - 发送请求
+    async editUser() {
+      const res = await this.$http.put(`users/${this.form.id}`,{
+        email:this.form.email,
+        mobile:this.form.mobile
+      });
+      // console.log(res);
+      const {meta:{msg,status}} = res.data
+      if(status === 200) {
+        // 关闭对话框
+        this.dialogFormVisibleEdit = false;
+        // 刷新表格
+        this.getTableData();
+      }else {
+        thi.$message.warning(msg);
+      }
+    },
+
+    // 编辑用户 - 打开对话框
+    showEditDia(user) {
+      this.form = user;
+      this.dialogFormVisibleEdit = true
+    },
 
     // 点击删除用户 - 打开提示框
     showDeleConfirm(user) {
@@ -168,6 +281,7 @@ export default {
 
     // 点击添加用户按钮 - 显示对话框
     showDiaAdd() {
+      // 打开对话框时，清空表单
       this.form = {}
       this.dialogFormVisibleAdd = true
     },
